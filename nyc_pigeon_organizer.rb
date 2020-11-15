da18506f15e352017b26a4fc5082ae88fc1c40e0
Daniel Sasse <dsasse07@gmail.com>
2020-11-15 21:39:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  pidgeon_names = []
+  
+  data.invert.keys[1].each_value {|value| pidgeon_names << value}
+  
+  binding.pry
 end
